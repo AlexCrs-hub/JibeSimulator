b0086a50741d2207b462cac987e423eb0917dd23
AlexCrs-hub <achi.grigore@gmail.com>
2025-08-26 22:22:20 +0300

    started learning project

---
+++ SensorSimulator/SensorSimulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using SensorSimulator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Sensor sensor1 = new Sensor("pressure");
+        Sensor sensor2 = new Sensor("temperature");
+        Sensor sensor3 = new Sensor("light");
+        Console.WriteLine(sensor1.sensorName);
+        Console.WriteLine(sensor2.sensorName);
+        Console.WriteLine(sensor3.sensorColor);
+    }
+}
---
+++ SensorSimulator/SensorSimulator/Sensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorSimulator
+{
+    public class Sensor
+    {
+        public string sensorName { get; set; }
+        public bool isRunning { get; set; }
+        public string sensorColor { get; set; }
+
+        public Sensor(string sensorName) {
+            this.sensorName = sensorName;
+            this.sensorColor = "gray";
+            this.isRunning = false;
+        }
+    }
+}
