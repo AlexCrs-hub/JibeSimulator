8bc57ab70162c6e8cdffec18b2bf3e90c6459c35
Alex <achi.grigore@gmail.com>
2025-09-01 12:24:30 +0300

    started webapp

---
+++ WebSimulator/WebSimulator/Models/HiveConnection.cs
@@ -0,0 +1,33 @@
+﻿namespace WebSimulator.Models
+{
+    public class HiveConnection
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public HiveConnection(string host, int port, string username, string password)
+        {
+            Host = host;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+
+        public HiveMQClient CreateClient()
+        {
+            var options = new HiveMQClientOptions
+            {
+                Host = Host,
+                Port = 8883,
+                UseTLS = true,
+                UserName = Username,
+                Password = Password,
+            };
+            var client = new HiveMQClient(options);
+
+            return client;
+        }
+    }
+}
